332095fb1ab8ca10381748f22c2a1870124f7e5a
Muata Nkosi <mnkosi@fandm.edu>
2019-06-30 01:59:41 -0400

    Read word list and append

--- script.js
+++ script.js
@@ -3,18 +3,32 @@
 // fetch(url)
 //     .then((response) => console.log(response)) //assuming file contains json
 //     .then((json) => console.log(json));
+let passwords;
 
 
-chrome.runtime.getPackageDirectoryEntry(function(root) {
-    root.getFile('diceware.wordlist.asc', {}, function(fileEntry) {
-        fileEntry.file(function(file) {
-        var reader = new FileReader();
-        reader.onload = function(e) {
-            // contents are in this.result
-            console.log(e.target.result);
-        };
-        console.log(reader.readAsText(file));
+function readFile(){
+    chrome.runtime.getPackageDirectoryEntry(function(root) {
+        root.getFile('diceware.wordlist.asc', {}, function(fileEntry) {
+            fileEntry.file(function(file) {
+            var reader = new FileReader();
+            reader.onload = function(e) {
+                // contents are in this.result
+                passwords = e.target.result.split('\n');
+                // console.log(passwords);
+                let wordList = [];
+                for (let i = 0; i < 7; i++) {
+                    console.log('here');
+                    console.log(passwords[Math.floor(Math.random()* 7467)].split(' '));
+                    wordList.push();
+                }
+                console.log(wordList);
+            };
+            reader.readAsText(file);
+            });
         });
     });
-    });
-    
+}
+
+readFile();
+readFile();
+readFile();
