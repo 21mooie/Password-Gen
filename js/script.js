02b0684ea2b332bfbb0b5a944479e030cfc99d90
Muata Nkosi <muatankosi1@gmail.com>
2021-05-02 01:57:21 -0400

    Implement rueusable way to add alerts to extension

--- js/script.js
+++ js/script.js
@@ -9,6 +9,18 @@ slider.oninput = function() {
 
 let entireFile;
 
+function showAlert(status, text) {
+    const messageDiv = document.getElementById('pageMessages');
+    const alert = document.createElement('div');
+    alert.className = `alert alert-${status} fade show`;
+    alert.setAttribute('role', 'alert');
+    alert.appendChild(document.createTextNode(text));
+    messageDiv.appendChild(alert);
+    setTimeout(function() {
+        messageDiv.removeChild(alert);
+    }, 2000); 
+}
+
 function getNewPassword(entireFile, allowCaps, allowNumbers, allowSpecialChars) {
     let password = '';
     while(password.length < slider.value) {
@@ -69,100 +81,26 @@ function readFile(allowCaps, allowNumbers, allowSpecialChars){
     });
 }
 
-function createAlert(title, summary, details, severity, dismissible, autoDismiss, appendToId) {
-    var iconMap = {
-      info: "fa fa-info-circle",
-      success: "fa fa-thumbs-up",
-      warning: "fa fa-exclamation-triangle",
-      danger: "fa ffa fa-exclamation-circle"
-    };
-  
-    var iconAdded = false;
-  
-    var alertClasses = ["alert", "animated", "flipInX"];
-    alertClasses.push("alert-" + severity.toLowerCase());
-  
-    if (dismissible) {
-      alertClasses.push("alert-dismissible");
-    }
-  
-    var msgIcon = $("<i />", {
-      "class": iconMap[severity] // you need to quote "class" since it's a reserved keyword
-    });
-  
-    var msg = $("<div />", {
-      "class": alertClasses.join(" ") // you need to quote "class" since it's a reserved keyword
-    });
-  
-    if (title) {
-      var msgTitle = $("<h4 />", {
-        html: title
-      }).appendTo(msg);
-      
-      if(!iconAdded){
-        msgTitle.prepend(msgIcon);
-        iconAdded = true;
-      }
-    }
-  
-    if (summary) {
-      var msgSummary = $("<strong />", {
-        html: summary
-      }).appendTo(msg);
-      
-      if(!iconAdded){
-        msgSummary.prepend(msgIcon);
-        iconAdded = true;
-      }
-    }
-  
-    if (details) {
-      var msgDetails = $("<p />", {
-        html: details
-      }).appendTo(msg);
-      
-      if(!iconAdded){
-        msgDetails.prepend(msgIcon);
-        iconAdded = true;
-      }
-    }
-    
-  
-    if (dismissible) {
-      var msgClose = $("<span />", {
-        "class": "close", // you need to quote "class" since it's a reserved keyword
-        "data-dismiss": "alert",
-        html: "<i class='fa fa-times-circle'></i>"
-      }).appendTo(msg);
-    }
-    
-    $('#' + appendToId).prepend(msg);
-    
-    if(autoDismiss){
-      setTimeout(function(){
-        msg.addClass("flipOutX");
-        setTimeout(function(){
-          msg.remove();
-        },1000);
-      }, 5000);
-    }
-  }
-
 document.getElementById('generatePasswordButton').addEventListener('click', (e) => {
     e.preventDefault();
     readFile(document.getElementById('caps').checked, document.getElementById('numbers').checked, document.getElementById('specialChars').checked);
 });
 
-document.getElementById('copy').addEventListener('click', () => {
+document.getElementById('copy').addEventListener('click', (e) => {
+    e.preventDefault();
     const copyText = document.getElementById('newPassword');
-    /* Select the text field */
-    copyText.select();
-
-    /* Copy the text inside the text field */
-    document.execCommand("copy");
-
-    createAlert('','Nice Work!','Here is a bunch of text about some stuff that happened.','success',true,true,'pageMessages');
-});
+    if (copyText.value) {
+        /* Select the text field */
+        copyText.select();
+        try {
+           /* Copy the text inside the text field */
+            document.execCommand("copy");
+            showAlert('success', 'Password saved to clipboard!');
+        } catch (err) {
+            showAlert('danger', 'Uh-oh something went wrong!');
+        }
+    }
+});  
 
 
 
